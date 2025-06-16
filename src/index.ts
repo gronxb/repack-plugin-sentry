92147106f28eecb12167ee7442a2fa9dc5fbcadb
gronxb <gron1gh1@gmail.com>
2025-06-16 22:26:57 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import type { Compiler, RspackPluginInstance } from "@rspack/core";
+import {
+	Compilation,
+	type Compiler,
+	type RspackPluginInstance,
+} from "@rspack/core";
 import { v4 as uuidv4 } from "uuid";
 
 const createDebugIdBanner = (debugId: string) => `
@@ -33,5 +37,42 @@ export class SentryDebugIdPlugin implements RspackPluginInstance {
 			raw: true,
 			footer: true,
 		}).apply(compiler);
+
+		compiler.hooks.thisCompilation.tap(
+			"AddDebugIdAfterSourceMapPlugin",
+			(compilation) => {
+				compilation.hooks.processAssets.tap(
+					{
+						name: "AddDebugIdAfterSourceMapPlugin",
+						stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
+					},
+					(assets) => {
+						for (const assetName of Object.keys(assets)) {
+							if (!assetName.endsWith(".map")) continue;
+
+							const asset = compilation.getAsset(assetName);
+							const source = asset?.source.source();
+
+							if (!source) {
+								continue;
+							}
+							try {
+								const mapJson = JSON.parse(source.toString());
+								mapJson.debug_id = debugId;
+								mapJson.debugId = debugId;
+								const updatedSource = JSON.stringify(mapJson);
+								compilation.updateAsset(
+									assetName,
+									new compiler.webpack.sources.RawSource(updatedSource),
+								);
+								console.log(`[debug_id] added to: ${assetName}`);
+							} catch (e) {
+								console.warn(`[debug_id] Failed to parse ${assetName} as JSON`);
+							}
+						}
+					},
+				);
+			},
+		);
 	}
 }
